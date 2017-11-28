9e4010aaabb1703c336edb2e6c3542808af4f8e9
Oakga Lwin <oakioakga@gmail.com>
2017-11-28 17:20:45 -0500

    finished auth flow

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
+const passport = require('passport');
+const cookieSession = require('cookie-session');
+
+require('./models/User');
 require('./services/passport');
 
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000, //in miliseconds so 30 days * 24 hours * 60 minutes * 60 seconds * 10 to the power 3
+        keys: [keys.cookieKey]
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
--- services/passport.js
+++ services/passport.js
@@ -1,14 +1,34 @@
 const passport = require('passport');
+const mongoose = require('mongoose');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});// turn the user into id
+
+passport.deserializeUser((id, done) => {
+    User.findById(id).then((user) => {
+        done(null, user);
+    });
+}); // turn id into moogoose collection
+
 
 passport.use(new GoogleStrategy({
     clientID: keys.googleClientID, //idenitfy ourself to google servers
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
 }, (accessToken, refreshToken, profile, done) => {
-    console.log(accessToken);
-    console.log(refreshToken);
-    console.log(profile);
-    console.log(done);
+    User.findOne({ googleID: profile.id }).then((existingUser)=> { if (existingUser) {
+        // we already have a record with a given profile id
+        done(null, existingUser );// no error here , existing user return
+    } else { 
+        // make a new record if the ID doesn't exist
+        new User({ googleID: profile.id }).save()// new model instance aka a record
+        .then(user => done(null, user));     // this user is more updated
+    };
+    });
 }));
+
